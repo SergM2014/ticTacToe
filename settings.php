40d4322bfc9d5a03a50e7e3e75ba887aa185c975
SergM2014 <weisse011@gmail.com>
2022-10-18 16:25:30 +0300

    all error screen output +

--- settings.php
+++ settings.php
@@ -4,6 +4,10 @@ declare(strict_types=1);
 
 session_start();
 
+ini_set('error_reporting', E_ALL);
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+
 require_once 'vendor/autoload.php';
 require_once 'routes.php';
 require_once 'viewHelpers.php';
