9cc8cf369516ebababf7cf26876cb96c430a34c8
SergM2014 <weisse011@gmail.com>
2022-10-21 16:00:33 +0300

    vue is plugged

--- resources/view/templates/footer.php
+++ resources/view/templates/footer.php
@@ -1,5 +1,12 @@
 
     </div>
 </body>
-<script src="/main.js?ver=<?=time() ?>"></script>
+<!-- <script src="/main.js?ver=<?=time() ?>"></script> -->
+<script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
+
+<script type="module">
+    import App from "/components/App.js";
+    Vue.createApp(App).mount('#app');
+
+</script>
 </html>
